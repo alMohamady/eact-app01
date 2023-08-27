6f60af5fb5cd2679808edcc3234ca035de21425b
al Mohamady <al.Mohamady47@gmail.com>
2023-08-28 02:07:02 +0300

    04 working with jsx

--- src/App.js
+++ src/App.js
@@ -2,22 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const str = 'world 2023 - 1';
+
+  function getName(name){
+     return <h1>Hello {name}</h1>
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          {getName(str)}
     </div>
   );
 }
