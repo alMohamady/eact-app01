ec1f8e713ce75af11e77cc2d1217438d7197b16b
al Mohamady <al.Mohamady47@gmail.com>
2023-08-30 10:37:26 +0300

    05 Rendering elements

---
+++ src/MyApp.js
@@ -0,0 +1,22 @@
+
+
+function MyApp() {
+   return (
+      <div className="App">
+        <h3>From MyApp</h3>
+      </div>
+   );
+}
+
+function OtherApp() {
+    return (
+       <div className="App">
+         <h3>From Other App</h3>
+       </div>
+    );
+ }
+
+ export {
+    MyApp ,
+    OtherApp
+ };
--- src/index.js
+++ src/index.js
@@ -2,12 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { MyApp, OtherApp } from './MyApp';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
-);
+
+function tick() {
+ const element = (
+    <div>
+        <h4>Time Now</h4>
+        <h5> { new Date().toLocaleTimeString() } </h5>
+    </div>
+ )
+ return root.render(
+    <>
+       <App />
+       <hr />
+       <MyApp />
+       <OtherApp />
+       <hr />
+       { element}
+    </>
+ );
+}
+
+setInterval(tick, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
