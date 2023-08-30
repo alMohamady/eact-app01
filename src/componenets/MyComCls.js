fdfc87b21fa43b9703bd81c65c922071447f5a7a
al Mohamady <al.Mohamady47@gmail.com>
2023-08-30 14:30:09 +0300

    10 State and Lifecycle II

--- src/componenets/MyComCls.js
+++ src/componenets/MyComCls.js
@@ -3,19 +3,33 @@ import React from "react";
 class MyComCls extends React.Component {
     
     constructor(props) {
+        console.log("constructor");
         super(props);
         this.state = { date: new Date() };
     }
 
+    /* static getDerivedStateFromProps() {
+        console.log("getDerivedStateFromProps");
+    }*/
+
+    shouldComponentUpdate(nextProps, nextState){
+        return true;
+    }
+
     render() {
-        console.log(this.props);
-       return (
+        //console.log(this.props);
+        console.log("render");
+        return (
         <>
           <h1>From React Class</h1>
           <h3> {this.props.name} : {this.props.age} : {this.props.xyz}  </h3>
           <h4> the time is  {this.state.date.toLocaleTimeString()} </h4>
         </>
        );
+    }
+
+    componentDidMount(){
+        console.log("componentDidMount");
     }
 }
 
