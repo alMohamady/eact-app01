c736a2d312ff44b2d04ec2836c9b3c28f94e3e33
al Mohamady <al.Mohamady47@gmail.com>
2023-09-08 23:27:01 +0300

    12 Using Event handler

--- src/componenets/MyComCls.js
+++ src/componenets/MyComCls.js
@@ -3,7 +3,7 @@ import React from "react";
 class MyComCls extends React.Component {
     
     constructor(props) {
-        console.log("constructor");
+        //console.log("constructor");
         super(props);
         this.state = {
             name: "AlMohamady",
@@ -22,22 +22,35 @@ class MyComCls extends React.Component {
 
     render() {
         //console.log(this.props);
+
+         const clcButton = () => {
+            console.log('hellow world');
+         }
+
         return (
         <>
           <h1>From React Class</h1>
           <h3> {this.props.name} : {this.props.age} : {this.props.xyz}  </h3>
           <h4> the time is  {this.state.date.toLocaleTimeString()} </h4>
           <h4> hi {this.state.name} </h4>
+          <button onClick={clcButton}
+                  onMouseEnter={()=> { console.log("enter") }} 
+                  onMouseLeave={()=> { console.log("Leave") }}
+                  >Button Test</button>
+          <br/>
+          <input type="text" onChange={(event) => {
+            console.log(event.target.value);
+          }} />
         </>
        );
     }
 
     componentDidMount(){
-        console.log("render");
+        //00console.log("render");
         this.setState({
             name: "Mo Salah"
         });
-        console.log("componentDidMount");
+        //console.log("componentDidMount");
     }
 }
 
